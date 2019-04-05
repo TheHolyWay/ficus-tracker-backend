6a5c849a9bd26e0780311bb2ceaa2914faadcb7f
disbal <vladislav.vlasov.dev@gmail.com>
2019-04-05 23:29:37 +0400

    Fix db connection

--- ficus-tracker/config.py
+++ ficus-tracker/config.py
@@ -3,6 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'postgresql://localhost/' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') + os.path.join(basedir, 'app.db') or \
+                              'postgresql://localhost' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
