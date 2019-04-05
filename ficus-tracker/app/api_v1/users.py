109f24bae8dd8f3e0799a723e395551e640fd610
disbal <vladislav.vlasov.dev@gmail.com>
2019-04-06 00:57:24 +0400

    Add simple user registration

--- ficus-tracker/app/api_v1/users.py
+++ ficus-tracker/app/api_v1/users.py
@@ -1,11 +1,44 @@
-from flask import jsonify
+import base64
 
+from flask import jsonify, request
+
+from app import db
 from app.api_v1 import bp
+from app.api_v1.errors import bad_request
+from app.models import User
 
 
 @bp.route('/users', methods=['POST'])
 def create_user_or_return_token():
-    pass
+    headers = request.headers or {}
+    resp_data = {}
+
+    # Check request
+    if 'Authorization' not in headers:
+        return bad_request('Must have authorization header')
+
+    # Parse auth
+    auth_str = headers.get('Authorization').split(' ')[1]
+    auth_str = base64.b64decode(auth_str).decode()
+    auth_str = auth_str.split(':')
+    login = auth_str[0]
+    password = auth_str[1]
+
+    if User.query.filter_by(login=login).first():
+        resp_data['message'] = "Уже есть такой юзер"
+
+    # Create user
+    user = User()
+    user.login = login
+    user.password_hash = user.set_password(password)
+    user.token = user.generate_token()
+    resp_data['token'] = user.token
+    db.session.add(user)
+    db.session.commit()
+
+    resp = jsonify(resp_data)
+
+    return resp
 
 
 @bp.route('/users/<int:id_>', methods=['GET'])
--- ficus-tracker/app/models.py
+++ ficus-tracker/app/models.py
@@ -1,17 +1,23 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    token = db.Column(db.String(128))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    @staticmethod
+    def generate_token():
+        return uuid.uuid4()
 
     def __repr__(self):
         return '<User {}>'.format(self.login)
