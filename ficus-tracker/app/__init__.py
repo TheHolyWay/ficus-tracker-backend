885fe1b3964ffe6dfffb3acabd623811b6b443a0
disbal <vladislav.vlasov.dev@gmail.com>
2019-04-05 22:30:43 +0400

    Fix ficus-tracker app running

--- ficus-tracker/app/__init__.py
+++ ficus-tracker/app/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask, Blueprint
 from config import Config
 
 
-main_bp = Blueprint('', __name__)
+main_bp = Blueprint('index', __name__)
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    app.register_blueprint('/', main_bp)
+    app.register_blueprint(main_bp, url_prefix='/')
 
     return app
 
