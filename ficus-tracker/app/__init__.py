92cf95042363a91da3d4d5c6bc18665aff1da6dc
disbal <vladislav.vlasov.dev@gmail.com>
2019-04-06 17:57:42 +0400

    Add tasks initialization during application start

--- ficus-tracker/app/__init__.py
+++ ficus-tracker/app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, Blueprint
+
+from app.recommendations.engine import RecommendationBackGroundTask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,6 +12,18 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     return "Welcome on the ficus-tracker-backend"
+
+
+def init_background_tasks():
+    registered_tasks = models.RecommendationItem.query.all()
+    for task in registered_tasks:
+        flower = models.Flower.query.filter_by(id=task.flower).first()
+        if not flower:
+            db.session.delete(task)
+            db.session.commit()
+        else:
+            recommendation_class = task.r_class
+            RecommendationBackGroundTask(recommendation_class.create_from_db(flower))
 
 
 app = Flask(__name__)
@@ -24,6 +38,6 @@ from app.api_v1 import bp as api_bp
 app.register_blueprint(main_bp, url_prefix='/')
 app.register_blueprint(api_bp, url_prefix='/api/v1')
 
+init_background_tasks()
+
 from app import models
-
-
