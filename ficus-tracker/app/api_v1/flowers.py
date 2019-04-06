e42c4efcd32f645b5233508f65ac49e63f7e2321
disbal <vladislav.vlasov.dev@gmail.com>
2019-04-06 11:25:36 +0400

    Add get flower endpoint

--- ficus-tracker/app/api_v1/flowers.py
+++ ficus-tracker/app/api_v1/flowers.py
@@ -3,8 +3,8 @@ from flask import request
 from app import db
 from app.api_v1 import bp
 from app.models import FlowerType, User, Flower
-from app.utils import create_response_from_data_with_code, parse_authorization_header
-from app.api_v1.errors import server_error, bad_request, error_response
+from app.utils import create_response_from_data_with_code, parse_authorization_header, authorize
+from app.api_v1.errors import server_error, bad_request, error_response, unauthorized
 import logging
 
 
@@ -48,11 +48,15 @@ def create_flower():
     except Exception as e:
         return server_error(f"Exception occurred during loading user: {str(e)}")
 
-    if user:
+    if user and authorize(login, password, user):
         data = request.get_json() or {}
         logging.info(data)
         if 'name' not in data or 'type' not in data:
             return bad_request(f"Request must includes name and type fields. Request: {data}")
+
+        if Flower.query.filter_by(name=data.get('name')).first():
+            return error_response(500, f"Flower with name {data.get('name')} already exists for "
+                                       f"user {login}")
 
         flower = Flower()
         flower.name = data.get('name')
@@ -65,4 +69,36 @@ def create_flower():
 
         return create_response_from_data_with_code(flower.to_dict(), 201)
     else:
-        return error_response(500, f"There is not user with username: {login}")
+        return unauthorized(login)
+
+
+# ToDo: Add support for sensor
+@bp.route(FLOWERS_API_PREFIX, methods=['GET'])
+def get_user_flowers():
+    """ Return list of user flowers """
+    logging.info("Called getting flowers endpoint ...")
+
+    headers = request.headers or {}
+
+    # Check request
+    if 'Authorization' not in headers:
+        return bad_request("Missing 'Authorization' header in request")
+
+    # Parse auth
+    try:
+        login, password = parse_authorization_header(headers.get('Authorization'))
+    except Exception as e:
+        return server_error(f"Exception occurred during parsing user credentials: {str(e)}")
+
+    try:
+        user = User.query.filter_by(login=login).first()
+    except Exception as e:
+        return server_error(f"Exception occurred during loading user: {str(e)}")
+
+    if user and authorize(login, password, user):
+        u_flowers = user.flowers.all()
+        return create_response_from_data_with_code(
+            list(map(lambda x: {'id': x.id}, list(u_flowers))), 200)
+    else:
+        return unauthorized(login)
+
